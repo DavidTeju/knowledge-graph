de36bd5be00972cbac8924dbfa7b515cdb37a5c3
Robel Schwarz <itsrobel.schwarz@gmail.com>
2025-02-23 00:01:48 -0500

    changes

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -9,11 +9,20 @@
 	transform="translate(-{size/2},-{size/2})"
 	class="place-content-between rounded-2xl bg-teal-500 p-2"
 	><div class="card">
-		<button
-			class="rounded bg-blue-500 p-5 font-bold text-white hover:bg-blue-700"
-			onclick={() => count++}
-		>
-			{id}: {count}
-		</button>
+		<div class="card bg-primary text-primary-content w-96 text-wrap">
+			<div class="card-body">
+				<h2 class="card-title">Card title!</h2>
+				<p>If a dog chews shoes whose shoes does he choose?</p>
+				<div class="card-actions justify-end">
+					<button class="btn">Buy Now</button>
+				</div>
+			</div>
+		</div>
+		<!-- <button -->
+		<!-- 	class="rounded bg-blue-500 p-5 font-bold text-white hover:bg-blue-700" -->
+		<!-- 	onclick={() => count++} -->
+		<!-- > -->
+		<!-- 	{id}: {count} -->
+		<!-- </button> -->
 	</div></foreignObject
 >
